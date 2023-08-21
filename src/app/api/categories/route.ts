5e592724115d087b4ddd0ffd99c45fb6a22288d6
RaphaelVR <raphaelventura55@gmail.com>
2023-08-21 23:40:55 +0200

    route & featured prod & prod

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,17 +1,41 @@
-import { prisma } from "@/utils/connect";
 import { NextResponse } from "next/server";
+import { prisma } from "@/utils/connect";
+// ALL CATEGORIES 
 
-
-// FETCH ALL CATEGORIES
 export const GET = async () => {
   try {
+
     const categories = await prisma.category.findMany();
-    return new NextResponse(JSON.stringify(categories), { status: 200 });
+    return new NextResponse(
+      JSON.stringify(categories),
+      { status: 200}
+    );
+
   } catch (err) {
     console.log(err);
     return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }),
+      JSON.stringify({ message: "Something went wrong!"}),
       { status: 500 }
     );
   }
 };
+
+
+// import { prisma } from "@/utils/connect";
+// import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
+
+
+// // FETCH ALL CATEGORIES
+// export const GET = async () => {
+//   try {
+//     const categories = await prisma.category.findMany();
+//     return new NextResponse(JSON.stringify(categories), { status: 200 });
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" }),
+//       { status: 500 }
+//     );
+//   }
+// };
