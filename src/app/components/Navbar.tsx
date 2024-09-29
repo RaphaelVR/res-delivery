e75a12c481f1e050d03ccaa85cce4056784d579e
RaphaelVR <raphaelventura55@gmail.com>
2024-09-29 13:42:16 +0200

    small fixes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       </div>
       {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/">Massimo</Link>
+        <Link href="/">Raphael&amps Restaurant</Link>
       </div>
       {/* MOBILE MENU */}
       <div className="md:hidden">
@@ -25,9 +25,9 @@ const Navbar = () => {
       </div>
       {/* RIGHT LINKS */}
       <div className="hidden md:flex gap-4 items-center justify-end flex-1">
-        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
-          <Image src="/phone.png" alt="" width={20} height={20} />
-          <span>123 456 78</span>
+        <div className="md:absolute lg:static flex right-3 items-center gap-2 cursor-pointer bg-orange-300 px-8 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20}/>
+          <span className="flex flex-col items-center left-3">123 456 789</span>
         </div>
         <UserLinks/>
         <CartIcon />
